d1f02568dee07932a7e07e729e252dbc87a7d06b
Sinopeal <vinceqin12169@gmail.com>
2026-05-15 18:03:57 +0800

    update

--- Scraping/cookies/cookies_loop.py
+++ Scraping/cookies/cookies_loop.py
@@ -16,6 +16,7 @@ def cookies_loop(
         list_key: str,
         interval: float = 1.0,
         threshold: int | None = None,
+        concurrency: int = 1,
 ) -> Callable[
     [Callable[..., Awaitable[Optional[dict]]]],
     Callable[..., Coroutine[Any, Any, None]],
@@ -25,27 +26,30 @@ def cookies_loop(
     ) -> Callable[..., Coroutine[Any, Any, None]]:
         @wraps(fetch_cookies)
         async def wrapper(*args: Any, **kwargs: Any) -> None:
-            while True:
-                try:
-                    if threshold:
-                        count = await redis.llen(list_key)
-                        if count >= threshold:
-                            await asyncio.sleep(interval)
-                            continue
-                    data = await fetch_cookies(*args, **kwargs)
-                    if data is not None:
-                        payload = {
-                            "data": data,
-                            "create_at": int(time.time()),
-                            "used_times": 0,
-                            "failed_times": 0,
-                        }
-                        logger.info(f"[{list_key}]Get cookies: {data}")
-                        await redis.lpush(list_key, json.dumps(payload, ensure_ascii=False))
-                except Exception as e:
-                    logger.error(e)
-                finally:
-                    await asyncio.sleep(interval)
+            async def worker(worker_id: int):
+                while True:
+                    try:
+                        if threshold:
+                            count = await redis.llen(list_key)
+                            if count >= threshold:
+                                await asyncio.sleep(interval)
+                                continue
+                        data = await fetch_cookies(*args, **kwargs)
+                        if data:
+                            payload = {
+                                "data": data,
+                                "create_at": int(time.time()),
+                                "used_times": 0,
+                                "failed_times": 0,
+                            }
+                            logger.info(f"[{list_key}][worker-{worker_id}]Fetched cookies: {data}")
+                            await redis.lpush(list_key, json.dumps(payload, ensure_ascii=False))
+                    except Exception as e:
+                        logger.error(f"[{list_key}][worker-{worker_id}]Failed to fetch cookies: {e}")
+                    finally:
+                        await asyncio.sleep(interval)
+
+            await asyncio.gather(*(worker(i)) for i in range(1, concurrency + 1))
 
         return wrapper
 
